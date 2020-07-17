d38e2f0b971886a893c35ea03c7dfa62348aee85
ShaoqinTang <46792914+ShaoqinTang@users.noreply.github.com>
2020-07-17 17:02:30 -0400

    Update Bayes.py

--- Bayes/src/Bayes.py
+++ Bayes/src/Bayes.py
@@ -8,7 +8,7 @@ def loadDataSet():
                  ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
                  ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
     classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not   
-    return postingList,classVec
+    return postingList
                  
 def createVocabList(dataSet):
     vocabSet = set([])  #create empty set
